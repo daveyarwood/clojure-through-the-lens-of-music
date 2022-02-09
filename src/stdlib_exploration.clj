04a7c31ac77bccc44c00f317d7421a840bc30304
Dave Yarwood <dave.yarwood@gmail.com>
2022-02-09 15:50:12 -0500

    Add support for filtering the functions by a substring
    Example usage:
    
      bin/random-clojure-core-functions :n 5 :search '"coll"'

--- src/stdlib_exploration.clj
+++ src/stdlib_exploration.clj
@@ -1,14 +1,44 @@
 (ns stdlib-exploration
   (:require [clojure.string :as str]))
 
-(def ^:const clojure-core-functions
+(def ^:const all-clojure-core-functions
   (vals (ns-publics 'clojure.core)))
 
-(defn random-clojure-core-function
-  []
-  (rand-nth clojure-core-functions))
+(defn- strings-in-composite
+  [x]
+  (cond
+    (string? x)
+    [x]
 
-(defn function-info-string
+    (or (symbol? x) (keyword? x))
+    [(name x)]
+
+    (map? x)
+    (concat (map strings-in-composite (keys x))
+            (map strings-in-composite (vals x)))
+
+    (coll? x)
+    (apply concat (map strings-in-composite x))))
+
+(defn clojure-core-functions
+  [& {:keys [search]}]
+  (->> all-clojure-core-functions
+       (filter (fn [function]
+                 (let [{:keys [arglists doc], fn-name :name} (meta function)]
+                   (or (not search)
+                       (str/includes? (name fn-name) search)
+                       (and doc (str/includes? doc search))
+                       (some
+                         #(str/includes? % search)
+                         (strings-in-composite arglists))))))))
+
+(defn random-clojure-core-functions
+  [& [{:keys [n] :as opts}]]
+  (->> (clojure-core-functions opts)
+       shuffle
+       (take (or n 1))))
+
+(defn- function-info-string
   [function]
   (let [{:keys [name arglists doc added]} (meta function)]
     (format "%s%s\n%s\n%s"
@@ -20,9 +50,7 @@
 
 (defn print-random-clojure-core-functions!
   "A CLI entrypoint to print information about random clojure.core functions."
-  [{:keys [n]}]
+  [opts]
   (let [function-infos
-        (repeatedly
-          (or n 1)
-          #(function-info-string (random-clojure-core-function)))]
+        (map function-info-string (random-clojure-core-functions opts))]
     (println (str/join "\n\n" function-infos))))
