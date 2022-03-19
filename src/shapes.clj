80296d861093d61f40619d3f58990ec693da6fb4
Dave Yarwood <dave.yarwood@gmail.com>
2022-03-19 18:53:24 -0400

    Improve shape examples

--- src/shapes.clj
+++ src/shapes.clj
@@ -5,14 +5,24 @@
 (def ● '●)
 
 (comment
-  ;; cycle, take, drop
+  ;; repeat
+  (repeat ■)
+  (repeat 10 ■)
+
+  ;; take
+  (take 3 (repeat ■))
+
+  ;; cycle
+  (cycle [■ ▲ ●])
   (take 10 (cycle [■ ▲ ●]))
 
-  (take 10 (drop 1 (cycle [■ ▲ ●])))
+  ;; drop
+  (drop 2 [■ ▲ ●])
+  (take 10 (drop 2 (cycle [■ ▲ ●])))
 
   ;; equivalent to the previous example
   (->> (cycle [■ ▲ ●])
-       (drop 1)
+       (drop 2)
        (take 10))
 
   ;; filter
